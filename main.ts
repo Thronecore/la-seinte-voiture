63c710d46cd171f6a29618902dfa962f1cf6e6e4
Thronecore <102539760+Thronecore@users.noreply.github.com>
2022-05-09 18:43:56 +0200

    Ajout de mode auto et manuel (le code auto n'est pas codé) et de commentaire pour aider à se repérer

--- main.ts
+++ main.ts
@@ -1,29 +1,58 @@
+/**
+ * Code du micro:bit voiture
+ */
+/**
+ * Code du micro:bit contrôleur
+ */
+/**
+ * <-- Code du mode manuel
+ */
+/**
+ * <-- Code du mode manuel
+ */
+/**
+ * <-- Code du mode manuel
+ */
+/**
+ * <-- Code du mode manuel
+ */
 radio.onReceivedNumber(function (receivedNumber) {
-    if (receivedNumber == 0) {
-        servos.P0.run(VITESSE)
-        basic.pause(10)
-        servos.P0.run(0)
-    }
-    if (receivedNumber == 1) {
-        servos.P1.run(VITESSE)
-        basic.pause(10)
-        servos.P1.run(0)
-    }
-    if (receivedNumber == 4) {
-        servos.P0.run(-40)
-        servos.P1.run(-40)
-        basic.pause(10)
-        servos.P0.run(0)
-        servos.P1.run(0)
-    }
-    if (receivedNumber == 2) {
-        if (VITESSE > 100) {
-            VITESSE = VITESSE + 5
+    if (Mode == 0) {
+        if (receivedNumber == 0) {
+            servos.P0.run(VITESSE)
+            basic.pause(10)
+            servos.P0.run(0)
         }
-    }
-    if (receivedNumber == 3) {
-        if (true) {
-            VITESSE = VITESSE - 10
+        if (receivedNumber == 1) {
+            servos.P1.run(VITESSE)
+            basic.pause(10)
+            servos.P1.run(0)
+        }
+        if (receivedNumber == 4) {
+            servos.P0.run(-40)
+            servos.P1.run(-40)
+            basic.pause(10)
+            servos.P0.run(0)
+            servos.P1.run(0)
+        }
+        if (receivedNumber == 2) {
+            if (VITESSE > 100) {
+                VITESSE = VITESSE + 5
+            }
+        }
+        if (receivedNumber == 3) {
+            if (true) {
+                VITESSE = VITESSE - 10
+            }
+        }
+        if (receivedNumber == 5) {
+            if (Mode == 0) {
+                Mode = 1
+                radio.sendString("A")
+            } else {
+                Mode = 0
+                radio.sendString("M")
+            }
         }
     }
 })
@@ -48,6 +77,12 @@ input.onGesture(Gesture.TiltLeft, function () {
         basic.pause(10)
     }
 })
+input.onButtonPressed(Button.AB, function () {
+    radio.sendNumber(5)
+})
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
+})
 input.onButtonPressed(Button.B, function () {
     radio.sendNumber(2)
 })
@@ -59,5 +94,15 @@ input.onGesture(Gesture.LogoDown, function () {
     }
 })
 let VITESSE = 0
+let Mode = 0
 radio.setGroup(1)
+Mode = 0
 VITESSE = 30
+/**
+ * <-- code du mode auto
+ */
+basic.forever(function () {
+    if (Mode == 1) {
+    	
+    }
+})
